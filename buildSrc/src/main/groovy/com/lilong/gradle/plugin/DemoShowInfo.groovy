33ce487bc0989939b171dff464a6ef9910a26cf8
lilong <lilong@zhangyue.com>
2018-04-23 20:35:04 +0800

    more

--- buildSrc/src/main/groovy/com/lilong/gradle/plugin/DemoShowInfo.groovy
+++ buildSrc/src/main/groovy/com/lilong/gradle/plugin/DemoShowInfo.groovy
@@ -7,7 +7,19 @@ class DemoShowInfo implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        project.logger.lifecycle("===project.name : ${project.name}===")
+
+        /** project名字*/
+        project.logger.lifecycle "===project.name : ${project.name}==="
+
+        /** project的相对路径*/
+        project.logger.lifecycle "===project.path : ${project.path}==="
+
+        /** project的*/
+        project.logger.lifecycle "===project.buildDir.path : ${project.buildDir.path}==="
+        project.logger.lifecycle "===project.buildDir.absolutePath : ${project.buildDir.absolutePath}==="
+        project.logger.lifecycle "===project.buildFile.absolutePath : ${project.buildFile.absolutePath}==="
+
+        project.logger.lifecycle "===project.parent.name : ${project.parent.name}==="
     }
 
 }
