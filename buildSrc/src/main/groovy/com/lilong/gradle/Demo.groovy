18e937fc4c003ca307b144dc95a339c3dfb23708
lilong <lilong@zhangyue.com>
2019-03-02 19:52:43 +0800

    more

--- buildSrc/src/main/groovy/com/lilong/gradle/Demo.groovy
+++ buildSrc/src/main/groovy/com/lilong/gradle/Demo.groovy
@@ -1,8 +1,9 @@
 package com.lilong.gradle
 
-import com.lilong.gradle.plugin.DemoAdjustApkOutput
+import com.lilong.gradle.plugin.DemoShowLifeCycle
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+
 // 插件的两大作用：
 // 1. 根据原始的project dsl对project进行操作
 // 2. 通过定义extension对原始的project dsl进行扩展，给project增加新的功能
@@ -10,10 +11,10 @@ class Demo implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-//        new DemoShowLifeCycle().apply(project)
+        new DemoShowLifeCycle().apply(project)
 //        new DemoShowInfo().apply(project)
 //        new DemoCreateExtension().apply(project)
 //        new DemoCreateTask().apply(project)
-        new DemoAdjustApkOutput().apply(project)
+//        new DemoAdjustApkOutput().apply(project)
     }
 }
